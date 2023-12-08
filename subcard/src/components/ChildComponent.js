11785ae3ecaa15ebe0a26a1ff085f50e05b643cc
mohanapriyabtech <mohana.priya@sparkouttech.com>
2023-12-08 11:56:07 +0530

    subcard created

---
+++ subcard/src/components/ChildComponent.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const ChildComponent = () => {
+  const [childInputValues,  setChildInputValues] = useState({});
+
+  // const socket = io(`http://192.168.29.78:4005/?id=656f0cdd2285517eeeefbad7`);
+
+  // useEffect(() => {
+  //   // ComponentDidMount: Connect to the socket server
+  //   socket.on('connect', () => {
+  //     console.log('Socket connected');
+  //   });
+
+  //   // Handle connection errors
+  //   socket.on('connect_error', (error) => {
+  //     console.error('Socket connection error:', error);
+  //   });
+
+  //   // Handle disconnection
+  //   socket.on('disconnect', (reason) => {
+  //     console.log('Socket disconnected:', reason);
+  //   });
+
+  //   // ComponentWillUnmount: Disconnect when the component is unmounted
+  //   return () => {
+  //     socket.disconnect();
+  //   };
+  // }, []); 
+
+  // useEffect(() => {
+  //   // Listen for the event to update childInputValues
+  //   socket.on('updateInputValues', (data) => {
+  //     console.log(data,"data")
+  //     setChildInputValues(data);
+  //   });
+  //   console.log(childInputValues,"childInputValues")
+
+  //   // Clean up the socket listener when the component unmounts
+  //   return () => {
+  //     socket.off('updateInputValues');
+  //   };
+  // }, [socket]);
+
+  return (
+    <div style={{ marginLeft: '20px' }}>
+      <h2>Child Component</h2>
+      {childInputValues ? (
+        <div>
+          {childInputValues.title && (
+            <div>
+              <label>Title:</label>
+              <input type="text" value={childInputValues.title} readOnly />
+            </div>
+          )}
+          {childInputValues.option && (
+            <div>
+              <label>Select Option:</label>
+              <input type="text" value={childInputValues.option} readOnly />
+            </div>
+          )}
+          {childInputValues.description && (
+            <div>
+              <label>Description:</label>
+              <input type="text" value={childInputValues.description} readOnly />
+            </div>
+          )}
+          {childInputValues.url && (
+            <div>
+              <label>URL:</label>
+              <input type="text" value={childInputValues.url} readOnly />
+            </div>
+          )}
+        </div>
+      ) : (
+        <div>
+          <label>No data available</label>
+        </div>
+      )}
+    </div>
+  );
+}  
+
+export default ChildComponent;
---
+++ subcard/src/components/ParentComponent.js
@@ -0,0 +1,72 @@
+import React, { useEffect,useState } from 'react';
+import io from 'socket.io-client';
+import ChildComponent from './ChildComponent'; // Assuming your ChildComponent file is imported
+
+const ParentComponent = () => {
+  const fieldNames = ['Title', 'Description', 'Selected Option', 'URL'];
+  const [inputValues, setInputValues] = useState(['', '', '', '']);
+
+  // Connect to the socket server
+  const socket = io(`http://192.168.29.78:4005/?id=656f0cdd2285517eeeefbad7`);
+
+  useEffect(() => {
+    // ComponentDidMount: Connect to the socket server
+    socket.on('connect', () => {
+      console.log('Socket connected');
+    });
+
+    // Handle connection errors
+    socket.on('connect_error', (error) => {
+      console.error('Socket connection error:', error);
+    });
+
+    // Handle disconnection
+    socket.on('disconnect', (reason) => {
+      console.log('Socket disconnected:', reason);
+    });
+
+    // ComponentWillUnmount: Disconnect when the component is unmounted
+    return () => {
+      socket.disconnect();
+    };
+  }, []); 
+
+  // Function to emit typing event and update inputValues
+  const handleInputChange = (index, value) => {
+    setInputValues((prevValues) => {
+      const newValues = [...prevValues];
+      newValues[index] = value;
+
+      // Emit the event to update child components
+      socket.emit('updateInputValues', {
+        title: newValues[0],
+        description: newValues[1],
+        option: newValues[2],
+        url: newValues[3]
+      });
+
+      return newValues;
+    });
+  };
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div style={{ marginRight: '20px' }}>
+        <h2>Parent Component</h2>
+        {inputValues.map((value, index) => (
+          <div key={index}>
+            <label>{`${fieldNames[index]}: `}</label>
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => handleInputChange(index, e.target.value)}
+            />
+          </div>
+        ))}
+      </div>
+      <ChildComponent inputValues={inputValues} />
+    </div>
+  );
+};
+
+export default ParentComponent;
